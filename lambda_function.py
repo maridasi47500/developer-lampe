695bdbbdd31324cef3e8269b267fbeb3f0af57d9
codeur-dissi-e-delodela <marie.bistroff@gmail.com>
2025-01-17 23:24:50 -0300

    hey

--- lambda_function.py
+++ lambda_function.py
@@ -3,12 +3,17 @@ from alexa.skills.smarthome import AlexaResponse
 import requests
 import colorsys
 
-
 aws_dynamodb = {}
 #          North America: https://api.amazonalexa.com/v3/events
 #Europe and India: https://api.eu.amazonalexa.com/v3/events
 #Far East and Australia: https://api.fe.amazonalexa.com/v3/events
 #
+client_id = "Your Client Id"
+# TODO: Update with your Client Secret for calling the LWA API.
+client_secret = "Your Client Secret"
+# TODO: Update with your Endpoint Id.
+endpoint_id = "device_id"
+
 gateway_endpoint = 'https://api.amazonalexa.com'
 malampe =  {"couleur":{
 "hue": 350.5,
@@ -18,11 +23,85 @@ malampe =  {"couleur":{
 "brightness":80,
 "temperature":6500,
 "power":"OFF",
-"endpoint_id":"xxxxxxxxxxxxxx",
+"endpoint_id":"xxxxxxx",
 "connectivity":{"value": "OK"}
 }
 
-access_token_from_amazon='xxxxxxxxxxxxxxx'
+access_token_from_amazon='xxxxx'
+def handle_accept_grant(alexa_request):
+    auth_code = alexa_request["directive"]["payload"]["grant"]["code"]
+    message_id = alexa_request["directive"]["header"]["messageId"]
+
+    # The Login With Amazon API for getting access and refresh tokens from an auth code.
+    lwa_token_url = "https://api.amazon.com/auth/o2/token"
+
+    data = urllib.parse.urlencode(
+        {
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+    ).encode("utf-8")
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    }
+
+    url_request = urllib.request.Request(lwa_token_url, data, headers, "POST")
+
+    try:
+        with urllib.request.urlopen(url_request) as response:
+            """
+            Response will contain the following:
+            - access_token: Used in the ChangeReports you send to the Alexa Event Gateway.
+            - refresh_token: Used to obtain a new access_token from LWA when this one expires.
+            - token_type: Expected token type is Bearer.
+            - expires_in: Number of seconds until access_token expires (expected to be 3600, or one hour).
+            """
+            lwa_tokens = json.loads(response.read().decode("utf-8"))
+
+            # TODO: Save the LWA tokens in a secure location, such as AWS Secrets Manager.
+            logger.info("Success!")
+            logger.info(f"access_token: {lwa_tokens['access_token']}")
+            logger.info(f"refresh_token: {lwa_tokens['refresh_token']}")
+            logger.info(f"token_type: {lwa_tokens['token_type']}")
+            logger.info(f"expires_in: {lwa_tokens['expires_in']}")
+    except HTTPError as http_error:
+        logger.error(f"An error occurred: {http_error.read().decode('utf-8')}")
+
+        # Build the failure response to send to Alexa
+        response = {
+            "event": {
+                "header": {
+                    "messageId": message_id,
+                    "namespace": "Alexa.Authorization",
+                    "name": "ErrorResponse",
+                    "payloadVersion": "3"
+                },
+                "payload": {
+                    "type": "ACCEPT_GRANT_FAILED",
+                    "message": "Failed to retrieve the LWA tokens from the user's auth code."
+                }
+            }
+        }
+    else:
+        # Build the success response to send to Alexa
+        response = {
+            "event": {
+                "header": {
+                    "namespace": "Alexa.Authorization",
+                    "name": "AcceptGrant.Response",
+                    "messageId": message_id,
+                    "payloadVersion": "3"
+                },
+                "payload": {}
+            }
+        }
+
+    logger.info(f"accept grant response: {json.dumps(response)}")
+
+    return response
 
 
 def lambda_handler(event, context):
@@ -72,10 +151,8 @@ def lambda_handler(event, context):
         if name == 'AcceptGrant':
             # Note: This sample accepts any grant request
             # In your implementation you would use the code and token to get and store access tokens
-            grant_code = event['directive']['payload']['grant']['code']
-            grantee_token = event['directive']['payload']['grantee']['token']
-            aar = AlexaResponse(namespace='Alexa.Authorization', name='AcceptGrant.Response')
-            return send_response(aar.get())
+            return handle_accept_grant(event)
+
 
     if namespace == 'Alexa.Discovery':
         if name == 'Discover':
